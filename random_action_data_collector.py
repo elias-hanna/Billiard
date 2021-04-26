a872a3e11c19d7b8b0fadd4d0af0b0a85a1cdd7d
elias-hanna <h.elias@hotmail.fr>
2021-04-26 16:24:31 +0200

    Cleaned code

--- random_action_data_collector.py
+++ random_action_data_collector.py
@@ -21,8 +21,6 @@ T = 1000
 
 data_in = np.zeros((499*N,6))
 data_out = np.zeros((499*N,4))
-# data_in = None
-# data_out = None
   
 # N episodes
 for i in range(N):
@@ -50,23 +48,9 @@ for i in range(N):
     if t != 0:
 
       data_out[i*t] = obs
-      # print(obs, " | data_out: ", data_out[N*(t-1)], " | index: ", N*i*(t-1))
       data_in[i*t] = np.concatenate((prev_action, prev_obs), axis=None)
-      # print(np.concatenate((prev_action, prev_obs)), " | data_in: ", data_in[N*(t-1)])
-      # if data_out is None:
-      #   data_out = obs
-
-      # else:
-      #   data_out_t = obs
-      #   data_out = np.vstack([data_out, data_out_t])
-      # if data_in is None:
-      #   data_in = np.concatenate((prev_action, prev_obs), axis=None)
-      # else:
-      #   data_in_t = np.concatenate((prev_action, prev_obs), axis=None)
-      #   data_in = np.vstack([data_in, data_in_t])
     if done:
       end_time = time.time()
-      # print(i, "th iteration, time taken to complete: ", (end_time - begin_time)/1000, "s")
       print("{:.1f}".format(i/N*100),"% done", end="\r")
       break
 
@@ -84,11 +68,8 @@ print(data_in_no_0s.shape)
 ## Input training data
 print("data output:\n", data_out)
 print(data_out.shape)
-data_out_no_0s = data_out[~np.all(data_in == 0, axis=1)]
+data_out_no_0s = data_out[~np.all(data_in == 0, axis=1)] # filter out the same lines as for data_in
 print("REMOVED 0:\n", data_out_no_0s)
 print(data_out_no_0s.shape)
 
 np.savez("cue_random_action_data_"+str(N)+"_ep", x=data_in_no_0s, y=data_out_no_0s)
-
-# np.savez("data_input", data_in_no_0s)
-# np.savez("data_output", data_out_no_0s)
